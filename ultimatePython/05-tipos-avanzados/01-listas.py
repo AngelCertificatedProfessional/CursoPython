769471879934d5154df13d96609ddd75b7070ff0
Liuts213 <angel.espinozatorres213@gmail.com>
2023-07-03 17:08:09 -0700

    Se agrega nuevos scripts al sistema

---
+++ ultimatePython/05-tipos-avanzados/01-listas.py
@@ -0,0 +1,11 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["chachito", "feliz"]
+palabrasFelices = ["chachito", "feliz", "Felipe", "alumno"]
+bloeans = ["chachito", "feliz", "Felipe", "alumno"]
+matriz = [[0, 1], [1, 0]]
+ceros = [0, 1] * 10  # multiplica el contenido por el valor
+alfanumerico = numeros + letras
+rango = list(range(1, 11))  # desde el uno hasta el 10
+chars = list("Hola mundo")
+print(chars)
